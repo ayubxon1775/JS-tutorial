5226a1b84965e08e908303d6132b0b00322d0695
Ayubxon <ayubxonaxmatxonov@gmail.com>
2024-06-21 19:39:33 +0500

    js tutorial advence

---
+++ js/script.js
@@ -0,0 +1,67 @@
+   
+  
+
+   //  2-dars Veriable
+
+// let number = 10;
+// const borderColor = "red";
+// console.log(number);
+
+// number = 20
+// console.log(number);
+// // borderColor = "blue"
+
+// //Old version
+// console.log(name);
+
+// var name 
+
+
+//      3-dars Qat'iy Rejim
+
+// 
+// "use strict";
+// var name = "ayubxon"
+// console.log(name);
+
+// name = "ayubxon";
+// console.log(name);
+// 
+
+//         4-dars   Data types Primitive
+
+let number = 5.6; //DAta type number
+// console.log(-8 / 0);// Infinity
+// console.log("ayub"/8);// NaN 
+
+   // String
+const name = "ayubxon";
+
+   // Boolean
+const isMerried = false;
+const isCircleEarth = true;
+
+    // Null yoq o'zgaruvchi
+// console.log(namsj);
+
+   // Undefined
+let und;
+// console.log(und);
+
+
+   // Object
+const thief = {
+// key: value
+    age: 23,
+    jacket: "black",
+    isLong: false,
+}
+
+console.log(thief.jacket);
+console.log(thief["jacket"]);
+
+    // Array
+//                0        1        2     3   index raqamlari
+const colors = ["red", "black", "green", 10 ]
+console.log(colors[0]);
+
